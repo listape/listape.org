bdc6ff9ba3c824deb30c0e6fd83776e257b435a5
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2018-12-25 10:11:37 -0200

    M  composer.json M  composer.lock M  controller/listCreate.php A  controller/testGet.php M  helper/csv.php

--- controller/listCreate.php
+++ controller/listCreate.php
@@ -1,5 +1,6 @@
 <?php
 helper('upload');
+helper("csv");
 model([
     'list'
 ]);
@@ -12,6 +13,11 @@ if(isset($file['errors'])){
 }else{
     if($file['size']>=3 && $file['size']<=1000000){
         //1. verifica se o csv é valido (tem que ter pelo menos 1 valor)
+        $array=csvToArray($file['temp']);
+        if(!$array){
+          $data['error']=true;
+          view('listCreateGet',$data);
+        }
         //2. salva no banco de dados
         $uid=getListUid();
         $data=[
@@ -23,7 +29,7 @@ if(isset($file['errors'])){
         $db->insert('lists',$data);
         //3. salva a lista no disco rigido
         $filename=ROOT.'csv/'.$uid;
-        if(uploadMove($file['temp'],$filename)){
+        if(arrayToCsv($array,$filename)){
             //4. pede para o usuário atualizar o nome da lista
             redirect('/list/'.$uid.'/update');
             //5. redireciona para a página da lista
--- helper/csv.php
+++ helper/csv.php
@@ -1,13 +1,15 @@
 <?php
-function arrayToCsv($filename){
-
+function arrayToCsv($array,$filename){
+  $Sheet=new Basic\Sheet();
+  return $Sheet->toSheet($array,$filename,'csv');
 }
 function csvToArray($filename){
-
-}
-function getCsvRange($filename){
-
-}
-function isValidCsv($filename){
-
+  $Sheet=new Basic\Sheet();
+  $array=$Sheet->toArray($filename,'csv');
+  $count=$array;
+  if($count>=1){
+    return $array;
+  }else{
+    return false;
+  }
 }
