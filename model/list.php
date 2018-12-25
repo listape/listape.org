1a2bedeef123c0d5102eb6bde2bef9c93e0e12c0
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2018-12-25 10:51:14 -0200

    M  model/list.php

--- model/list.php
+++ model/list.php
@@ -1,68 +1,69 @@
 <?php
 function downloadList($uid,$data){
-    $file = ROOT.'csv/'.$uid;
-    $name='Capitais brasileiras.csv';
-    //$name=slug($name);
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.$name.'"');
-    header('Content-Transfer-Encoding: binary');
-    readfile($file);
+  $list=getListUid($uid);
+  $file = ROOT.'csv/'.$uid;
+  $name=$list['name'].'.csv';
+  //$name=slug($name);
+  header('Content-Description: File Transfer');
+  header('Content-Type: application/octet-stream');
+  header('Content-Disposition: attachment; filename="'.$name.'"');
+  header('Content-Transfer-Encoding: binary');
+  readfile($file);
 }
 function fetchAllListsByUuid($uuid,$db){
-    $where=[
-        'uuid'=>$uuid
-    ];
-    return $db->select('lists','*',$where);
+  $where=[
+    'uuid'=>$uuid
+  ];
+  return $db->select('lists','*',$where);
 }
 function getListByUid($uid){
-    $where=[
-        'uid'=>$uid
-    ];
-    return db()->get('lists','*',$where);
+  $where=[
+    'uid'=>$uid
+  ];
+  return db()->get('lists','*',$where);
 }
 function getListUid(){
-    $uid=getToken();
-    $where=[
-        'uid'=>$uid
-    ];
-    $exists=db()->get('list',"*",$where);
-    if($exists){
-        getListUid();
-    }else{
-        return $uid;
-    }
+  $uid=getToken();
+  $where=[
+    'uid'=>$uid
+  ];
+  $exists=db()->get('list',"*",$where);
+  if($exists){
+    getListUid();
+  }else{
+    return $uid;
+  }
 }
 
 function listUpdate($uid,$name,$uuid){
-    $name=printable($name);
-    if(strlen($name)>=3){
-        $where=[
-            'AND'=>[
-                'uid'=>$uid,
-                'uuid'=>$uuid
-            ]
-        ];
-        $db=db();
-        $data=$db->get("lists",'*',$where);
-        $data['name']=$name;
-        $data['updated_at']=time();
-        $result=$db->update("lists",$data,$where);
-        if($result){
-            redirect('/list/'.$uid);
-        }else{
-            die("erro ao atualizar o nome");
-        }
+  $name=printable($name);
+  if(strlen($name)>=3){
+    $where=[
+      'AND'=>[
+        'uid'=>$uid,
+        'uuid'=>$uuid
+      ]
+    ];
+    $db=db();
+    $data=$db->get("lists",'*',$where);
+    $data['name']=$name;
+    $data['updated_at']=time();
+    $result=$db->update("lists",$data,$where);
+    if($result){
+      redirect('/list/'.$uid);
     }else{
-        $data['error']=true;
-        view('listUpdateGet',$data);
+      die("erro ao atualizar o nome");
     }
+  }else{
+    $data['error']=true;
+    view('listUpdateGet',$data);
+  }
 }
 function searchListByName($name,$db){
-    $name=printable($name);
-    $where=[
-        'name[~]'=>$name
-    ];
-    return $db->select('lists','*',$where);
+  $name=printable($name);
+  $where=[
+    'name[~]'=>$name
+  ];
+  return $db->select('lists','*',$where);
 }
 ?>
